703311589770dac780c6e16f0a7e3257aebcc4c6
OwinoMichael <owinomichael1400@gmail.com>
2026-03-18 23:33:42 -0400

    AI Review controller

---
+++ backend-spring/src/main/java/com/mikeo/plasso/features/aiReview/AIController.java
@@ -0,0 +1,12 @@
+package com.mikeo.plasso.features.aiReview;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("ai-analysis")
+public class AIController {
+
+
+}
---
+++ backend-spring/src/main/java/com/mikeo/plasso/features/aiReview/GeminiService.java
@@ -0,0 +1,7 @@
+package com.mikeo.plasso.features.aiReview;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class GeminiService {
+}
